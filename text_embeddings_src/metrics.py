e5f1aa25275ae4231f355bc4205bc6906bd6a858
ritagonmar <ritagonzalez97@gmail.com>
2025-01-30 16:00:54 +0100

    metrics.py - eliminate knn_recall - make knn_accuracy like in the iclr_dataset repo - do the same with logistic_accuracy and rename it to linear_accuracy (also in nb 04) - adapt knn_accuracy_whitening_scores so that it actually uses the function of knn_accuracy of accepting X as a list of several datasets
    model_stuff.py
    - eliminate train_loop_eval_test_loss
    - eliminate train_loop_tsne_and_knn_rec
    
    nb 04
    - rename logistic_accuracy as linear_accuracy

--- text_embeddings_src/metrics.py
+++ text_embeddings_src/metrics.py
@@ -8,51 +8,71 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 
 
-def knn_accuracy(embeddings, true_labels, test_size=0.1, k = 10, rs=42, set_numpy = True, metric="euclidean"):
-    """Calculates kNN accuracy.
-    In principle should do the same as the function above, but the way of selecting the train and test set is differently.
-    Actually, if you use the same random seed as in the function above it gives you the same result (at least for the default parameters of train_test_split sklearn version 1.0.2).
-    
+def knn_acc(embeddings, labels, test_embeddings = None, test_labels = None,  test_size=0.1, k = 10, rs=42, metric="euclidean"):
+    """Computes kNN accuracy.
     
     Parameters
     ----------
-    embeddings : list 
-        List with the different datasets for which to calculate the kNN accuracy.
-    true_labels : array-like
-        Array with labels (colors).
+    embeddings : array-like
+        Full data if `test_embeddings`=None, or only train split if test split is also passed in `test_embeddings`.
+    labels : array-like
+        Full labels if `test_embeddings`=None, or only train split if test split is also passed in `test_labels`.
+    test_embeddings  : array-like, default = None
+        Test set (optional, if not passed a test set will be created).
+    test_labels : array-like, default = None
+        Test split of the labels (optional, if not passed a test split will be created).
+    test_size :  float, default=0.1
+        Fraction of the data used for the test set.
     k : int, default=10
         Number of nearest neighbors to use.
     rs : int, default=42
         Random seed.
+    metric :  string, {"euclidean", "cosine"}
+        Metric used to compute distances to build the kNN graph. You can pass whatever KNeighborsClassifier from sklearn accepts.
     
     Returns
     -------
-    knn_accuracy : float
-        kNN accuracy of the dataset.
+    knn_accuracy_score : float
+        kNN accuracy.
     
     """
-    
+    # check whether both test_embeddings and test_labels were not passed
+    assert (
+        not test_embeddings is None or test_labels is None
+    ), "You did not pass a test set (so the data will be split in train/test sets), but you passed a test split for the labels."
+
+
     random_state = np.random.seed(rs)
 
-    if type(embeddings) == list:
-        knn_accuracy = []
-        for embed in embeddings:
-            X_train, X_test, y_train, y_test = train_test_split(embed, true_labels, test_size=test_size, random_state = random_state)
-    
-            knn = KNeighborsClassifier(n_neighbors=k, algorithm='brute', n_jobs=-1, metric=metric)
-            knn = knn.fit(X_train, y_train)
-            knn_accuracy.append(knn.score(X_test, y_test))
-        if set_numpy == True:
-            knn_accuracy= np.array(knn_accuracy)
-        
+    # create/assign test splits
+    if test_embeddings is None:
+        X_train, X_test, y_train, y_test = train_test_split(embeddings, labels, test_size=test_size, random_state = random_state)
     else:
-        X_train, X_test, y_train, y_test = train_test_split(embeddings, true_labels, test_size=test_size, random_state = random_state)
-        knn = KNeighborsClassifier(n_neighbors=k, algorithm='brute', n_jobs=-1, metric=metric)
-        knn = knn.fit(X_train, y_train)
-        knn_accuracy = knn.score(X_test, y_test)
+        X_train, X_test, y_train, y_test = embeddings, labels, test_embeddings, test_labels
 
+    knn = KNeighborsClassifier(n_neighbors=k, algorithm='brute', n_jobs=-1, metric=metric)
+    knn = knn.fit(X_train, y_train)
+    knn_accuracy_score =  knn.score(X_test, y_test)
+    return knn_accuracy_score
+
+
+def knn_accuracy(embeddings, labels, test_embeddings = None, test_labels = None, test_size=0.1, k = 10, rs=42, metric="euclidean"):
+    """
+    Expand knn_acc function to also accept several datasets (with the same labels) to evaluate, passed as a list of datasets [X1,X2,...,XN].
+    It returns the accuracies in one list: for a single dataset as one single value, for several datasets as several values in the same list.
+    """
+
+    if not isinstance(embeddings, list):
+        embeddings = [embeddings]
+        
+    knn_accuracy = []
+    for embed in embeddings:
+        knn_accuracy.append(knn_acc(embed, labels, test_embeddings = test_embeddings, test_labels = test_labels, test_size=test_size, k = k, rs=rs, metric=metric))
+        
+    knn_accuracy= np.array(knn_accuracy)
     
     return knn_accuracy
+
 
 
 
@@ -82,144 +102,55 @@ def knn_accuracy_whitening_scores(X, y, rs=42):
 
     scores = np.zeros((3, 2))
 
-    for i, X_to_use in enumerate([X, Xcentered, Xwhitened]):
-        for j, metric in enumerate(["euclidean", "cosine"]):
+    for j, metric in enumerate(["euclidean", "cosine"]):
 
-            acc = knn_accuracy(X_to_use, y, metric=metric)
-            scores[i, j] = acc
+        acc = knn_accuracy([X, Xcentered, Xwhitened], y, metric=metric)
+        scores[:, j] = acc
 
     return scores
 
 
 
-def knn_recall(X, Z, k=10, test_size=0.1, rs=42):
-    """Calculates kNN recall. 
-    Calculates the kNN recall for `k`nearest neighbors.
+
+def lin_acc(embeddings, labels, test_embeddings = None, test_labels = None,  test_size=0.1, rs=42):
+    """Computes linear accuracy.
     
     Parameters
     ----------
-    X : {array-like, sparse matrix}
-        High-dimensional data.
-    Z : array-like
-        List of different low-dimensional data. When computing for only one low-dimensional dataset, still needs to be a list of array-like: [Z].
-    k : int, default=10
-        Number of nearest eeighbors.
-    subset_size : int, optional
-        Size of the subset of the data, if desired.
-    
-    Returns
-    -------
-    knn_recall : array like 
-        KNN recall
-    
-    See Also
-    --------
-    knn_recall_affinity_matrix, knn_recall_and_ratios
-    
-    
-    Note
-    ----
-    KNN recall is by definition the fraction of preserved nearest neighbors from the high-dimensional version of the dataset to the low-dimensional one.
-
-    """
-    
-    if test_size is not None:
-        _, X_test, _, Z_test = train_test_split(X, Z, test_size=test_size, random_state = rs)
-        
-        # In this case we will have to query k+1 points, because
-        # sklearn returns the query point itself as one of the neighbors
-        k_to_query = k + 1
-    else:
-        # In this case we can query k points
-        k_to_query = k 
-    
-    nbrs1 = NearestNeighbors(n_neighbors=k_to_query, algorithm='brute', n_jobs=-1).fit(X)
-    ind1 = nbrs1.kneighbors(X=None if test_size is None else X_test,
-                            return_distance=False)
-        
-    knn_recall = []
-    #for num, Z in enumerate(Zs):
-    print('.', end='')
-    nbrs2 = NearestNeighbors(n_neighbors=k_to_query, algorithm='brute', n_jobs=-1).fit(Z)
-    ind2 = nbrs2.kneighbors(X=None if test_size is None else Z_test,
-                            return_distance=False)
-
-    intersections = 0.0
-    for i in range(ind1.shape[0]):
-        intersections += len(set(ind1[i]) & set(ind2[i]))
-    
-    if test_size is None:
-        knn_recall.append(intersections / ind1.shape[0] / k)
-    else:
-        # it substracts the ind1.shape[0] because when you take a subset of the data
-        # in the NearestNeighbors.kneighbors function, it takes the query point itself as
-        # one of the neighbors, so you need to substract the intersection of a point with himself
-        knn_recall.append((intersections - ind1.shape[0]) / ind1.shape[0] / k)
-    
-    
-    return knn_recall
-
-
-def logistic_accuracy(
-    embeddings, true_labels, test_size=0.1, rs=42, set_numpy=True
-):
-    """Calculates logistic accuracy.
-
-    Parameters
-    ----------
-    embeddings : list
-        List with the different datasets for which to calculate the kNN accuracy.
-    true_labels : array-like
-        Array with labels (colors).
-    test_size : float
-        Fraction of the data to take as test set.
+    embeddings : array-like
+        Full data if `test_embeddings`=None, or only train split if test split is also passed in `test_embeddings`.
+    labels : array-like
+        Full labels if `test_embeddings`=None, or only train split if test split is also passed in `test_labels`.
+    test_embeddings  : array-like, default = None
+        Test set (optional, if not passed a test set will be created).
+    test_labels : array-like, default = None
+        Test split of the labels (optional, if not passed a test split will be created).
+    test_size :  float, default=0.1
+        Fraction of the data used for the test set.
     rs : int, default=42
         Random seed.
-
+    
     Returns
     -------
-    accuracy : float
-        Accuracy of the logistic classifier in the test set.
-
+    linear_accuracy : float
+        Linear accuracy.
+    
     """
+    # check whether both test_embeddings and test_labels were not passed
+    assert (
+        not test_embeddings is None or test_labels is None
+    ), "You did not pass a test set (so the data will be split in train/test sets), but you passed a test split for the labels."
+
 
     random_state = np.random.seed(rs)
 
-    if type(embeddings) == list:
-        accuracy = []
-        for embed in embeddings:
-            X_train, X_test, y_train, y_test = train_test_split(
-                embed,
-                true_labels,
-                test_size=test_size,
-                random_state=random_state,
-            )
-            lr = make_pipeline(
-                StandardScaler(),
-                LogisticRegression(
-                    penalty=None,
-                    solver="saga",
-                    tol=1e-2,
-                    random_state=random_state,
-                    n_jobs=-1,
-                    max_iter=1000,
-                ),
-            )
-
-            lr.fit(X_train, y_train)
-            accuracy.append(lr.score(X_test, y_test))
-        if set_numpy == True:
-            accuracy = np.array(accuracy)
-
+    # create/assign test splits
+    if test_embeddings is None:
+        X_train, X_test, y_train, y_test = train_test_split(embeddings, labels, test_size=test_size, random_state = random_state)
     else:
-        X_train, X_test, y_train, y_test = train_test_split(
-            embeddings,
-            true_labels,
-            test_size=test_size,
-            random_state=random_state,
-        )
+        X_train, X_test, y_train, y_test = embeddings, labels, test_embeddings, test_labels
 
-        lr = make_pipeline(
+    log_reg = make_pipeline(
             StandardScaler(),
             LogisticRegression(
                 penalty=None,
@@ -230,8 +161,25 @@ def logistic_accuracy(
                 max_iter=1000,
             ),
         )
+    
+    log_reg.fit(X_train, y_train)
+    linear_accuracy_score = log_reg.score(X_test, y_test)
+    return linear_accuracy_score
 
-        lr.fit(X_train, y_train)
-        accuracy = lr.score(X_test, y_test)
 
-    return accuracy
+def linear_accuracy(embeddings, labels, test_embeddings = None, test_labels = None, test_size=0.1, rs=42):
+    """
+    Expand lin_acc function to also accept several datasets (with the same labels) to evaluate, passed as a list of datasets [X1,X2,...,XN].
+    It returns the accuracies in one list: for a single dataset as one single value, for several datasets as several values in the same list.
+    """
+
+    if not isinstance(embeddings, list):
+        embeddings = [embeddings]
+        
+    lin_accuracy = []
+    for embed in embeddings:
+        lin_accuracy.append(lin_acc(embed, labels, test_embeddings = test_embeddings, test_labels = test_labels, test_size=test_size, rs=rs))
+        
+    lin_accuracy= np.array(lin_accuracy)
+    
+    return lin_accuracy
