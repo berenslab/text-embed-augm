add embedding normalizations. Fix difference in normalization between SentenceTransformer wrapper and my custom wrapper

text_embeddings_src/embeddings.py
@@ -59,15 +59,18 @@ def generate_embeddings(
             batch = {k: v.to(device) for k, v in batch.items()}
             out = model(**batch)
             token_embeds = out[0]  # get the last hidden state
-            # ENH: here would come the normalization
             av = mean_pool(token_embeds, batch["attention_mask"])
+            av = torch.nn.functional.normalize(av, p=2, dim=1)
             sep = sep_pool(token_embeds, batch["attention_mask"])
+            sep = torch.nn.functional.normalize(sep, p=2, dim=1)
             cls = cls_pool(token_embeds, batch["attention_mask"])
+            cls = torch.nn.functional.normalize(cls, p=2, dim=1)
             embedding_av.append(av.detach().cpu().numpy())
             embedding_sep.append(sep.detach().cpu().numpy())
             embedding_cls.append(cls.detach().cpu().numpy())
             if return_seventh == True:
                 seventh = token_embeds[:, 7, :]
+                seventh = torch.nn.functional.normalize(seventh, p=2, dim=1)
                 embedding_7th.append(seventh.detach().cpu().numpy())
 
     embedding_av = np.vstack(embedding_av)
@@ -142,13 +145,17 @@ def generate_embeddings_embed_layer(
             out = model(batch["input_ids"])
             token_embeds = out  # [0]  # get the last hidden state
             av = mean_pool(token_embeds, batch["attention_mask"])
+            av = torch.nn.functional.normalize(av, p=2, dim=1)
             sep = sep_pool(token_embeds, batch["attention_mask"])
+            sep = torch.nn.functional.normalize(sep, p=2, dim=1)
             cls = cls_pool(token_embeds, batch["attention_mask"])
+            cls = torch.nn.functional.normalize(cls, p=2, dim=1)
             embedding_av.append(av.detach().cpu().numpy())
             embedding_sep.append(sep.detach().cpu().numpy())
             embedding_cls.append(cls.detach().cpu().numpy())
             if return_seventh == True:
                 seventh = token_embeds[:, 7, :]
+                seventh = torch.nn.functional.normalize(seventh, p=2, dim=1)
                 embedding_7th.append(seventh.detach().cpu().numpy())
 
     embedding_av = np.vstack(embedding_av)
@@ -165,7 +172,7 @@ def generate_embeddings_embed_layer(
     )
 
 
-# TODO: this function has not been adapted to the new code
+# TODO: this function has not been adapted to the new code. Add normalization when adapting it.
 @torch.no_grad()
 def generate_embeddings_hidden_state(
     layer_number,

text_embeddings_src/models.py
@@ -98,11 +98,6 @@ class CustomModelMTEBWrapper:
         Returns:
             The encoded sentences.
         """
-        # print(len(inputs))
-        # print(type(inputs))
-        # print(kwargs.keys())
-        # print(kwargs.values())
-        # print(kwargs["batch_size"])
 
         device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -137,7 +132,9 @@ class CustomModelMTEBWrapper:
             out = self.model(**batch, output_hidden_states=True)
             token_embeds = out.hidden_states[self.layer_number]
             embd = self.pooler(token_embeds, batch["attention_mask"])
-            # embd = torch.nn.functional.normalize(embd, p=2, dim=1)  # FIX to results not matching those when using the SentenceTransformer wrapper
+            embd = torch.nn.functional.normalize(
+                embd, p=2, dim=1
+            )  # FIX for the difference with SentenceTransformers
             embeddings.append(embd.detach().cpu())
 
         embeddings = torch.cat(embeddings, dim=0)
@@ -411,6 +408,9 @@ class MyEmbeddingSentenceModel:
                 }  # it used to be device only (not self.device) but I think it only wroked bc of jupyter having device defined somewhere else
                 outputs = self.model(batch["input_ids"])
                 embdd = self.pooler(outputs, batch["attention_mask"])
+                embd = torch.nn.functional.normalize(
+                    embd, p=2, dim=1
+                )  # normalizes embeddings
                 embeddings.append(embdd.detach().cpu().numpy())
 
         embeddings = np.vstack(embeddings)